25f04da3e9787dcdeda53391e72b64ebbc1c7be8
Falcon.G2 <ctw.vanderpluijm@student.avans.nl>
2024-12-17 14:53:18 +0100

    Added interface for coming HTTPS client

---
+++ HUELampenOpdracht2/HUELampen.Domain/ClientInterface/IHTTPSClient.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HUELampen.Domain.ClientInterface
+{
+    public interface IHTTPSClient
+    {
+        Task ConnectAsync();
+        Task<string> ReceiveDataAsync();
+        Task SendAsync(string data);
+        Task CloseAsync();
+    }
+}
